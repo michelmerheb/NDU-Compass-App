46c9dcd20406261192b601720e31fbc590ad1eeb
Michel Merheb <merhebmichel980@gmail.com>
2025-01-26 19:53:03 +0200

    ...

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { initializeAuth} from "firebase/auth";
+import * as firebaseAuth from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID} from "@env";
+
+const reactNativePersistence = (firebaseAuth as any).getReactNativePersistence;
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAde5zWPXzLspgrcMpTp-b7IsJH8UnHQng",
+  authDomain: "student-helper-1.firebaseapp.com",
+  projectId: "student-helper-1",
+  storageBucket: "student-helper-1.firebasestorage.app",
+  messagingSenderId: "640658223704",
+  appId: "1:640658223704:web:170c42f1bde8fbd129902d"
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: reactNativePersistence(AsyncStorage)
+});
+export const FIREBASE_STORAGE = getStorage(FIREBASE_APP);
